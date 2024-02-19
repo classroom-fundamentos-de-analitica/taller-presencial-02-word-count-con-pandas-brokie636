ed72fdbd5dcbb8d61e80c291b7716da86c6bed33
brokie636 <104879191+brokie636@users.noreply.github.com>
2024-02-19 09:50:35 -0500

    Actualizar word_count.py

--- word_count.py
+++ word_count.py
@@ -1,3 +1,4 @@
+
 """Taller evaluable"""
 
 import glob
@@ -7,7 +8,11 @@ import pandas as pd
 
 def load_input(input_directory):
     """Load text files in 'input_directory/'"""
-    #
+    filenames = glob.glob(input_directory + "/*.*")
+    print(filenames)
+    dataframes = [pd.read_csv(filename, sep =";", names=["text"]) for filename in filenames]
+    dataframe = pd.concat(dataframes).reset_index(drop=True)
+    return dataframe
     # Lea los archivos de texto en la carpeta input/ y almacene el contenido en
     # un DataFrame de Pandas. Cada línea del archivo de texto debe ser una
     # entrada en el DataFrame.
@@ -16,6 +21,11 @@ def load_input(input_directory):
 
 def clean_text(dataframe):
     """Text cleaning"""
+    dataframe = dataframe.copy()
+    
+    dataframe['text'] = dataframe['text'].str.lower()
+    dataframe['text'] = dataframe['text'].str.replace(",", "").str.replace(".","")
+    return dataframe
     #
     # Elimine la puntuación y convierta el texto a minúsculas.
     #
@@ -23,18 +33,30 @@ def clean_text(dataframe):
 
 def count_words(dataframe):
     """Word count"""
-
+    dataframe = dataframe.copy()
+    dataframe['text'] = dataframe['text'].str.split()
+    dataframe = dataframe.explode('text').reset_index(drop=True)
+    dataframe['count'] = 1
+    dataframe = dataframe.rename(columns={'text':'word'}
+    conteo = dataframe.groupby(['text', as_index=False]).agg(
+     {
+       'value': sum
+     }
+    return conteo
 
 def save_output(dataframe, output_filename):
     """Save output to a file."""
-
+    dataframe.to_csv(output_filename, index=False, sep="\t", header=False)
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
 def run(input_directory, output_filename):
     """Call all functions."""
-
+    dataframe = load_input(input_directory)
+    dataframe = clean_text(dataframe)
+    dataframe = count_words(dataframe)
+    print(dataframe)
 
 if __name__ == "__main__":
     run(
